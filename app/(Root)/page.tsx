2261da5e30e94db0d836db87398a259ecb99c18c
NKC27 <93439579+NKC27@users.noreply.github.com>
2024-08-15 10:39:36 +0100

    implement the complete layout

--- app/(Root)/page.tsx
+++ app/(Root)/page.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: 'Nick',
+    lastName: 'Clarkeston',
+    email: 'nkc@nkc.co.uk',
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={12550.89}
+          />
+        </header>
+        Recent transactions
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 3562.56 }, { currentBalance: 8562.56 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
